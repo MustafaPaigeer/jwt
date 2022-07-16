a051c962cfec8d7485140e0c660c75442adbd10b
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-16 13:17:48 -0700

    setup authentication error

--- controllers/main.js
+++ controllers/main.js
@@ -4,20 +4,20 @@ const CustomAPIError = require('../errors/custom-error')
 
 
 const login = async (req, res) => {
-  const {username, password} = req.body
+  const { username, password } = req.body
 
-  if(!username || !password) {
+  if (!username || !password) {
     throw new CustomAPIError('Please provide email and password', 400)
   }
   const id = new Date().getDate();
-  const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
 
-  res.status(200).json({msg: 'user created',token})
+  res.status(200).json({ msg: 'user created', token })
 }
 
-const dashboard = async (req,res) => {
+const dashboard = async (req, res) => {
   const authHeader = req.headers.authorization;
-  
+
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     throw new CustomAPIError('No token provided', 401)
   }
@@ -27,9 +27,9 @@ const dashboard = async (req,res) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     console.log(decoded)
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
-  }catch (err) {
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
+  } catch (err) {
     throw new CustomAPIError('Not authorized to access this route', 401)
   }
 
