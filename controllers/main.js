1fc2db5e5052b21393e166e1c768738cc2d16f5a
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-16 10:56:55 -0700

    validate jwt token

--- controllers/main.js
+++ controllers/main.js
@@ -9,13 +9,20 @@ const login = async (req, res) => {
   if(!user || !password) {
     throw new CustomAPIError('Please provide email and password', 400)
   }
-  
-  const token = jwt.sign()
+  const id = new Date().getDate;
+  const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
 
-  res.send('Fake Login/Register/Signup Route')
+  res.send.status(200).json({msg: 'user created',token})
 }
 
 const dashboard = async (req,res) => {
+  const authHeader = req.headers.authorization;
+  
+  if (!authHeader || !authHeader.startWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401)
+  }
+
+
   const luckyNumber = Math.floor(Math.random()*100)
   res.status(200).json({msg: `Hello, John Doe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
